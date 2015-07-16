65c35c913d512a8fd36cf7ee9419aa9228ffdc6b
juhuntenburg <ju.huntenburg@gmail.com>
2015-07-16 19:22:56 +0200

    adding symmetric colormap

--- nilearn/plotting/surf_plotting.py
+++ nilearn/plotting/surf_plotting.py
@@ -6,11 +6,11 @@ Only matplotlib is required.
 # display figure? return figure?
 # scaling of figure
 # new function for rois
+# why symmtric_cbar = False?
 # symmetric colormap -> _get_plot_stat_map_params
 # colorbar -> _get_plot_stat_map_params
 
 from nilearn._utils.compat import _basestring
-from .img_plotting import _get_plot_stat_map_params
 
 # Import libraries
 import numpy as np
@@ -22,9 +22,11 @@ from mpl_toolkits.mplot3d import Axes3D
 
 # function to figure out datatype and load data
 def load_surf_data(filename):
-    if filename.endswith('nii') or filename.endswith('nii.gz') or filename.endswith('mgz'):
+    if (filename.endswith('nii') or filename.endswith('nii.gz') or
+            filename.endswith('mgz')):
         data = np.squeeze(nib.load(filename).get_data())
-    elif filename.endswith('curv') or filename.endswith('sulc') or filename.endswith('thickness'):
+    elif (filename.endswith('curv') or filename.endswith('sulc') or
+            filename.endswith('thickness')):
         data = nib.freesurfer.io.read_morph_data(filename)
     elif filename.endswith('annot'):
         data = nib.freesurfer.io.read_annot(filename)[0]
@@ -35,10 +37,50 @@ def load_surf_data(filename):
     return data
 
 
-# plotting of surface with optional background and stats map
+def _get_plot_surf_params(stat_map_data, vmax, symmetric_cbar,
+                          kwargs, force_min_stat_map_value=None):
+    """ Internal function for setting value limits for plot_surf
+    """
+    # make sure that the color range is symmetrical
+    if vmax is None or symmetric_cbar in ['auto', False]:
+        stat_map_max = np.nanmax(stat_map_data)
+        if force_min_stat_map_value is None:
+            stat_map_min = np.nanmin(stat_map_data)
+        else:
+            stat_map_min = force_min_stat_map_value
+    if symmetric_cbar == 'auto':
+        symmetric_cbar = stat_map_min < 0 and stat_map_max > 0
+    if vmax is None:
+        vmax = max(-stat_map_min, stat_map_max)
+    if 'vmin' in kwargs:
+        raise ValueError('This function does not accept a "vmin" '
+                         'argument, as it uses a symmetrical range '
+                         'defined via the vmax argument. To threshold '
+                         'the map, use the "threshold" argument')
+    vmin = -vmax
+    if not symmetric_cbar:
+        negative_range = stat_map_max <= 0
+        positive_range = stat_map_min >= 0
+        if positive_range:
+            cbar_vmin = 0
+            cbar_vmax = None
+        elif negative_range:
+            cbar_vmax = 0
+            cbar_vmin = None
+        else:
+            cbar_vmin = stat_map_min
+            cbar_vmax = stat_map_max
+    else:
+        cbar_vmin, cbar_vmax = None, None
+    return cbar_vmin, cbar_vmax, vmin, vmax
+
+
 def plot_surf(mesh, hemi, stat_map=None, bg_map=None, threshold=None,
               view='lateral', cmap='RdBu_r', alpha='auto', vmax=None,
               symmetric_cbar="auto", output_file=None, **kwargs):
+
+    """ Plotting of surfaces with optional background and stats map
+    """
 
     # load mesh and derive axes limits
     coords, faces = nib.freesurfer.io.read_geometry(mesh)
@@ -57,7 +99,6 @@ def plot_surf(mesh, hemi, stat_map=None, bg_map=None, threshold=None,
         else:
             raise ValueError('view must be one of lateral, medial, '
                              'dorsal or ventral')
-
     elif hemi == 'lh':
         if view == 'medial':
             elev, azim = 0, 0
@@ -70,7 +111,6 @@ def plot_surf(mesh, hemi, stat_map=None, bg_map=None, threshold=None,
         else:
             raise ValueError('view must be one of lateral, medial, '
                              'dorsal or ventral')
-
     else:
         raise ValueError('hemi must be one of rh or lh')
 
@@ -125,25 +165,28 @@ def plot_surf(mesh, hemi, stat_map=None, bg_map=None, threshold=None,
                                  'rois or labels use plot_roi_surf instead')
             stat_map_values = np.mean(stat_map_data[faces], axis=1)
 
+            cbar_vmin, cbar_vmax, vmin, vmax = \
+                _get_plot_surf_params(stat_map_values, vmax,
+                                      symmetric_cbar, kwargs)
+
             if threshold is not None:
                 kept_indices = np.where(abs(stat_map_values) >= threshold)[0]
-                stat_map_values = stat_map_values - stat_map_values.min() #vmin
-                stat_map_values = stat_map_values / stat_map_values.max() #vmax-vmin
+                stat_map_values = stat_map_values - vmin
+                stat_map_values = stat_map_values / (vmax-vmin)
                 face_colors[kept_indices] = cmap(stat_map_values[kept_indices])
             else:
-                stat_map_values = stat_map_values - stat_map_values.min()
-                stat_map_values = stat_map_values / stat_map_values.max()
+                stat_map_values = stat_map_values - vmin
+                stat_map_values = stat_map_values / (vmax-vmin)
                 face_colors = cmap(stat_map_values)
 
         plot_mesh.set_facecolors(face_colors)
-    plot_mesh.set_edgecolors = 'none'
 
     # save figure if output file is given
     if output_file is not None:
         plt.savefig(output_file)
 
-#m = plt.cm.ScalarMappable(cmap=plt.cm.RdBu_r)
-#m.set_array(stat_map_values[kept_indices])
-#cbar = plt.colorbar(m)
+# m = plt.cm.ScalarMappable(cmap=plt.cm.RdBu_r)
+# m.set_array(stat_map_values[kept_indices])
+# cbar = plt.colorbar(m)
 
     plt.show()
